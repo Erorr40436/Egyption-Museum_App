62acea0275e0cdff6c80f15127cd8b4b2586fdf7
Ibrahim Ragab <iragab068l@gmail.com>
2026-07-16 16:32:32 +0300

    initialization

---
+++ lib/core/style/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6200EE);
+  static const Color appBarTitleColor = Color(0xFFD4AF37);
+  static const Color gold50 = Color(0xFFF2CA50);
+  static const Color gold49 = Color(0xFFEAC249);
+  static const Color descriptionTextColor = Color(0xFFD0C5AF);
+  static const Color whiteColor = Color(0xFFE5E2E1);
+  static const Color bcgColor = Color(0xFF4D4635);
+  static const Color bcgFavCardColor = Color(0xFF1C1B1B);
+}
---
+++ lib/core/style/app_textstlyles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:museum/core/constants/app_fonts.dart';
+
+class AppTextstlyles {
+  static TextStyle appBarTitle = TextStyle(
+    fontSize: 20,
+    fontFamily:AppFonts.libration,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+}
